4707b5c58b5caad55931754b163e1523ce905f00
Darragh <darraghmckay@gmail.com>
2020-04-21 08:13:23 +0100

    Move to component based IsometricRoutes for multiple examples

--- src/data/blog/IsometricIllusions.js
+++ src/data/blog/IsometricIllusions.js
@@ -2,8 +2,9 @@ import React, { useEffect, useRef, useState } from 'react';
 import { Color, Point } from 'isomer';
 import IsomerRoute from 'isomer-route';
 import DIR from 'isomer-route/src/directions';
-import { blue, red } from 'isomer-route/src/colors';
-import { useEventListener, useAnimationFrame } from './utils/hooks';
+import { blue, darkGreen, red } from 'isomer-route/src/colors';
+import IsometricRoute from '../../components/IsometricRoute';
+import { useEventListener } from '../../utils/hooks';
 
 const green = new Color(60, 160, 50);
 const GRID_SIZE = 16;
@@ -11,7 +12,7 @@ const GRID_SIZE = 16;
 const randomInt = (max = 6, min = 1) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
 
-const generateRoute = (route, lengths = 60) => {
+const generateRoute = (route, lengths = 30) => {
   let lengthsX = lengths;
   let direction = route.direction;
   while (lengthsX > 0) {
@@ -38,7 +39,6 @@ const generateRoute = (route, lengths = 60) => {
           generateRoute(
             new IsomerRoute(route.canvas, route.origin)
               .addStairs(stairLength, stairDir)
-              // .addPath(1, stairDir)
               .updateOrigin(
                 stairDir === DIR.Y ? -2 : 0,
                 stairDir === DIR.X ? -2 : 0,
@@ -66,24 +66,16 @@ const generateRoute = (route, lengths = 60) => {
 };
 
 const IsometricIllusions = () => {
-  const canv = useRef(null);
   let gameRoute = useRef(null);
+  let generatedRoute = useRef(null);
   const [rotation, setRotation] = useState(0);
   const [nextRotation, setNextRotation] = useState(0);
-  const [bloxRotation, setBloxRotation] = useState(0);
   const [generated, setGenerated] = useState(false);
-  const [time, setTime] = useState(0);
   const [blox, setBlox] = useState({
     origin: Point(0, 0, 1),
     direction: DIR.X,
     standing: false,
   });
-
-  const moveAndRotateBlox = (x, y, z = 0) =>
-    setBlox({
-      ...blox,
-      origin: Point(blox.origin.x + x, blox.origin.y + y, blox.origin.z + z),
-    });
 
   const setBloxOrigin = origin =>
     setBlox({
@@ -91,19 +83,10 @@ const IsometricIllusions = () => {
       origin,
     });
 
-  const clearCanvas = () => {
-    const context = canv.current.getContext('2d');
-    context.clearRect(0, 0, canv.current.width, canv.current.height);
-  };
-
   const generateNewPath = () => {
     setGenerated(true);
-    clearCanvas();
     setBloxOrigin(Point(0, 0, 1));
-    const route = new IsomerRoute(canv.current).drawGrid(GRID_SIZE);
-    const randomRoute = generateRoute(route);
-    gameRoute.current = randomRoute;
-    randomRoute.draw();
+    generatedRoute.current = null;
   };
 
   const handleKeyPress = event => {
@@ -156,157 +139,220 @@ const IsometricIllusions = () => {
     }
   };
 
-  useAnimationFrame(timeDelta => {
-    setTime(time + timeDelta * 0.1);
-  });
-
   useEffect(() => {
     const rotationDelta = nextRotation - rotation;
     if (Math.abs(rotationDelta) > 0.015) {
       setRotation(
-        currentRotation => currentRotation + Math.PI * 0.01 * rotationDelta,
+        currentRotation => currentRotation + Math.PI * 0.05 * rotationDelta,
       );
     } else if (rotationDelta !== 0) {
       setRotation(nextRotation);
     }
-  }, [nextRotation, rotation, time]);
+  }, [nextRotation, rotation]);
 
-  useEffect(() => {
-    if (canv.current && !generated) {
-      clearCanvas();
-      const route = new IsomerRoute(canv.current, Point(0, 0, 0));
-      gameRoute.current = route;
+  const drawGameRoute = route => {
+    route
+      .rotate(rotation)
+      .setDelay(0)
+      .addPath(6, DIR.X)
+      .addPath(3, DIR.Y)
+      .addPath(3, DIR.X)
+      .addPath(3, DIR.Y);
 
-      // route
-      //   .setRotation(rotation)
-      //   .addPath(2, DIR.X)
-      //   .setOrigin(Point(0, 1, 0))
-      //   .addPath(2, DIR.X)
-      //   .setOrigin(Point(0, 0, 1))
-      //   .addPath(2, DIR.X)
-      //   .setOrigin(Point(0, 1, 1))
-      //   .addPath(2, DIR.X);
+    const route2 = route.split().addStairs(3, DIR.X);
 
-      // route
-      //   .setRotation(rotation)
-      //   .addPath(2, DIR.X)
-      //   .setOrigin(Point(0, -2, 2))
-      //   .addPath(2, DIR.X);
+    route.addShapes(route2.split().addStairs(3, DIR.X).shapes);
 
-      // route
-      //   .setRotation(rotation)
-      //   .setDelay(0)
-      //   .addPath(2, DIR.X, red)
-      //   .setOrigin(Point(1, 0, 1))
-      //   .addPath(2, DIR.Y, green)
-      //   .setOrigin(Point(0, 1, 0))
-      //   .addColumn(2, DIR.UP);
+    route.addShapes(
+      route2
+        .addPath(6, DIR.Y)
+        .addPath(8, DIR.X)
+        .addPath(-7, DIR.Y)
+        .addPath(-3, DIR.X).shapes,
+    );
 
-      // route
-      //   .setRotation(rotation)
-      //   .setOrigin(Point(0, 4, 0))
-      //   .addPath(-2, DIR.Y, red)
-      //   .setOrigin(Point(0, 2, 1))
-      //   .addPath(2, DIR.X, green)
-      //   .setOrigin(Point(1, 3, 0))
-      //   .addColumn(2, DIR.UP);
+    const route3 = route2
+      .split()
+      .addPath(-3, DIR.Y)
+      .addPath(-4, DIR.X)
+      .addPath(-3, DIR.Y)
+      .addPath(-4, DIR.X);
 
-      // route
-      //   .setOrigin(Point(2, 1, 2))
-      //   .addPath(-2, DIR.X, red)
-      //   .setOrigin(Point(0, 2, 3))
-      //   .addPath(-2, DIR.Y, green)
-      //   .setOrigin(Point(1, 0, 2))
-      //   .addColumn(2, DIR.UP);
+    route.addShapes(route3.split().addStairs(3, DIR.Y).shapes);
 
-      // route
-      //   .setOrigin(Point(3, 0, 0))
-      //   .addPath(2, DIR.Y, red)
-      //   .setOrigin(Point(4, 1, 1))
-      //   .addPath(-2, DIR.X, green)
-      //   .setOrigin(Point(2, 0, 0))
-      //   .addColumn(2, DIR.UP);
+    route.addShapes(
+      route3
+        .addPath(-1, DIR.X)
+        .addPath(-2, DIR.Y)
+        .addPath(-5, DIR.X).shapes,
+    );
 
-      // route.drawGrid(GRID_SIZE).draw();
+    route
+      .addPath(4, DIR.Y)
+      .addColumn(10, DIR.DOWN)
+      .drawGrid(GRID_SIZE);
 
-      // new IsomerRoute(canv.current, Point(0, 0, 0))
-      //   .setRotation(rotation)
-      //   .setOrigin(Point(1, 3, 0))
-      //   .addPath(2, DIR.X, green)
-      //   .draw();
-
+    route.addShapes(
       route
-        .rotate(rotation)
-        .setDelay(0)
-        .addPath(6, DIR.X)
-        .addPath(3, DIR.Y)
-        .addPath(3, DIR.X)
-        .addPath(3, DIR.Y);
-
-      const route2 = route.split().addStairs(3, DIR.X);
-
-      route.addShapes(route2.split().addStairs(3, DIR.X).shapes);
-
-      route.addShapes(
-        route2
-          .addPath(6, DIR.Y)
-          .addPath(8, DIR.X)
-          .addPath(-7, DIR.Y)
-          .addPath(-3, DIR.X).shapes,
-      );
-
-      const route3 = route2
         .split()
-        .addPath(-3, DIR.Y)
-        .addPath(-4, DIR.X)
-        .addPath(-3, DIR.Y)
-        .addPath(-4, DIR.X);
+        .setOrigin(blox.origin)
+        .addPath(1, blox.direction, red).shapes,
+    );
 
-      route.addShapes(route3.split().addStairs(3, DIR.Y).shapes);
+    route.draw();
+    gameRoute.current = route;
+  };
 
-      route.addShapes(
-        route3
-          .addPath(-1, DIR.X)
-          .addPath(-2, DIR.Y)
-          .addPath(-5, DIR.X).shapes,
-      );
-
-      route
-        .addPath(4, DIR.Y)
-        .addColumn(10, DIR.DOWN)
-        .drawGrid(GRID_SIZE);
-
-      route
-        .addShapes(
-          route
-            .split()
-            .setOrigin(blox.origin)
-            .addPath(1, blox.direction, red).shapes,
-        )
-        .draw();
-    } else if (generated) {
-      clearCanvas();
-      new IsomerRoute(canv.current)
-        .rotate(rotation)
-        .addShapes(gameRoute.current.shapes)
-        .addShapes(
-          gameRoute.current
-            .split()
-            .setOrigin(blox.origin)
-            .addPath(1, blox.direction, red).shapes,
-        )
-        .drawGrid(GRID_SIZE)
-        .draw();
+  const drawGeneratedRoute = route => {
+    if (!generatedRoute.current) {
+      generatedRoute.current = generateRoute(route);
     }
-  }, [canv, blox, bloxRotation, gameRoute, generated, rotation]);
+
+    route
+      .setRotation(rotation)
+      .addShapes(generatedRoute.current.shapes)
+      .drawGrid(GRID_SIZE)
+      .draw();
+  };
+
   useEventListener('keydown', handleKeyPress);
 
   return (
     <div className="mx-auto  text-center flex flex-col">
+      <div id="isometric-triangle">
+        <IsometricRoute
+          height={600}
+          width={672}
+          drawRoute={route => {
+            route
+              .setGridSize(6)
+              .setRotation(rotation)
+              .updateOrigin(5, 5, 0)
+              .addColumn(6, DIR.UP)
+              .addPath(-6, DIR.Y)
+              .addPath(-6, DIR.X)
+              .draw();
+          }}
+        />
+      </div>
+      <div id="isometric-floating-blocks">
+        <IsometricRoute
+          height={300}
+          width={672}
+          scale={2}
+          drawRoute={route => {
+            route
+              .setGridSize(2)
+              .setRotation(rotation)
+              .addPath(2, DIR.X)
+              .setOrigin(Point(0, 1, 0))
+              .addPath(2, DIR.X)
+              .setOrigin(Point(0, 0, 2))
+              .addPath(2, DIR.X)
+              .setOrigin(Point(0, 1, 2))
+              .addPath(2, DIR.X)
+              .draw();
+          }}
+        />
+      </div>
+      <div id="isometric-conundrum">
+        <IsometricRoute
+          height={300}
+          width={672}
+          scale={2}
+          drawRoute={route => {
+            route
+              .setDelay(0)
+              .setGridSize(2)
+              .setRotation(rotation)
+              .addPath(2, DIR.X, red)
+              .setOrigin(Point(1, 0, 1))
+              .addPath(2, DIR.Y, green)
+              .setOrigin(Point(0, 1, 0))
+              .addColumn(2, DIR.UP)
+              .draw();
+          }}
+        />
+      </div>
+
+      {/* <div id="isometric-game">
+        <IsometricRoute
+          scale={4}
+          height={600}
+          width={672}
+          drawRoute={drawGameRoute}
+        />
+      </div> */}
+      <div id="isometric-monument-valley">
+        <IsometricRoute
+          scale={4}
+          height={600}
+          width={672}
+          drawRoute={route => {
+            route
+              .setGridSize(12)
+              .setOrigin(Point(3, -1, 1))
+              .addExtrusion(
+                [
+                  { x: 0, y: 0, z: 0 },
+                  { x: 1, y: 0, z: 0 },
+                  { x: 1, y: 1, z: 0 },
+                ],
+                0.2,
+                blue,
+                shape =>
+                  shape
+                    .rotateY(Point(3.5, -0.5, 1.5), (Math.PI * 1) / 2)
+                    .rotateX(Point(3.5, -0.5, 1.5), (-Math.PI * 3) / 2),
+              )
+              .updateOrigin(0, 0, 1)
+              .addColumn(2, DIR.UP)
+              .addColumn(5, DIR.UP, darkGreen, shape =>
+                shape.rotateY(Point(3.5, 4, 8.5), rotation),
+              )
+              .updateOrigin(0, 1, -1)
+              .addPath(4, DIR.Y, darkGreen, shape =>
+                shape.rotateY(Point(3.5, 4, 8.5), rotation),
+              )
+              .addPath(5, DIR.Y)
+              .updateOrigin(0, -1, 0)
+              .addStairs(3, DIR.X)
+              .updateOrigin(0, -1, 0)
+              .addPath(3, DIR.Y)
+              .updateOrigin(1, -1, 0)
+              .addPath(-2, DIR.Y)
+              .setOrigin(Point(-3, -2, 9))
+              .addPath(-4, DIR.X)
+              .addPath(-9, DIR.Y)
+              .addColumn(2, DIR.UP)
+              .addExtrusion(
+                [
+                  { x: 0, y: 0, z: 0 },
+                  { x: 1, y: 0, z: 0 },
+                  { x: 1, y: 1, z: 0 },
+                ],
+                1,
+                blue,
+                shape =>
+                  shape
+                    .rotateY(Point(-6.5, -11, 11.5), Math.PI / 2)
+                    .rotateX(Point(-6.5, -10.5, 11.5), -Math.PI / 2),
+              )
+              .draw();
+          }}
+        />
+      </div>
       <button className="mx-auto flex" onClick={generateNewPath}>
         Generate
       </button>
-      <canvas ref={canv} id="isometric-illusion" width="2688" height="3200" />
+      {/* <div id="isometric-generated-game">
+        <IsometricRoute
+          scale={4}
+          height={600}
+          width={672}
+          drawRoute={drawGeneratedRoute}
+        />
+      </div> */}
     </div>
   );
 };
